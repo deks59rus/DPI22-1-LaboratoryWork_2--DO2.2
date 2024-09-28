67ea09a3b6715aa40db3e3eaa24a65a2a0d5ba29
deks59rus <den10galka@mail.ru>
2024-09-28 12:43:14 +0500

    Простейшие алгебраические операции в новой ветке
    В файле func.py добавлены функции выполняющие сложение, вычитание, умножения и деления.

---
+++ func.py
@@ -0,0 +1,29 @@
+def add(a,b):
+    """
+    :param a:
+    :param b:
+    :return: сумма чисел
+    """
+    return a+b
+
+def sub(a,b):
+    """
+    :param a:
+    :param b:
+    :return: разность чисел
+    """
+    return a-b
+def mul(a,b):
+    """
+    :param a:
+    :param b:
+    :return: произведение чисел
+    """
+    return a*b
+def div(a,b):
+    """
+    :param a:
+    :param b:
+    :return: частное чисел чисел
+    """
+    return a/b
